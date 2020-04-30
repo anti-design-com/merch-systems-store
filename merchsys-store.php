d98033b173e7bde80d86ae2df316321cf438340c
Lars Lorenz <lars@anti-design.com>
2020-04-30 21:57:23 +0200

    MERCHSYS-55: version zpdate

--- merchsys-store.php
+++ merchsys-store.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: merch.systems Storefront
-Version: 1.0.5
+Version: 1.0.6
 Description: Fully integrates your merch.systems online store into your Wordpress website
 Plugin URI: https://merch.systems
 Author: anti-design.com GmbH & Co. KG
